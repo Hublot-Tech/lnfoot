7c9f025807139093252cc0a5536f688e1bb4e48e
Belrick Stephane <73307766+latioms@users.noreply.github.com>
2025-05-21 17:30:55 +0100

    feat: Add LiveScores component to display live match scores and status

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Ads from "@/components/Home/Ads";
 import GetApp from "@/components/Home/GetApp";
 import Hero from "@/components/Home/Hero";
 import LatestNews from "@/components/Home/LatestNews";
+import LiveScores from "@/components/Home/live-scores";
 import PointsForts from "@/components/Home/PointsForts";
 import ShopCarousel from "@/components/Home/ShopCarousel";
 
@@ -10,6 +11,7 @@ export default function Home() {
     <main className="min-h-screen justify-between">
       <Hero />
       <LatestNews /> 
+      <LiveScores />
       <PointsForts />
       <ShopCarousel />
       <Ads />

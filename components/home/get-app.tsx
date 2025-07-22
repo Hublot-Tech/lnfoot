fbfccb5f021d786ee9804d3813ff6423e77cc3f6
Marco Kuidja <kuidjamarco@gmail.com>
2025-07-22 12:37:19 +0100

    fix: updating the application download link

--- components/home/get-app.tsx
+++ components/home/get-app.tsx
@@ -1,33 +1,40 @@
 import React from 'react'
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 export default function GetApp() {
-	return (
-		<section className="py-20">
-			<div className='flex items-center justify-center bg-[url("https://shadcnblocks.com/images/block/patterns/circles.svg")] bg-cover bg-center py-20 text-center md:p-20'>
-				<div className="container">
-					<div className="mx-auto max-w-screen-md">
-						<h1 className="mb-4 text-3xl font-semibold text-blue-800  md:text-5xl">
-							Votre boutique de foot est desormais dans votre poche
-						</h1>
-						<p className="md:text-lg">
-							Découvrez notre collection d&apos;articles de foot et de sport directement depuis
-							votre smartphone. Maillots, chaussures, équipements... Commandez facilement
-							et profitez de nos offres exclusives sur l&apos;application !
-						</p>
-						<div className="mt-11 flex flex-col justify-center gap-2 sm:flex-row">
-							<a href="https://s3.ln-foot.com/app-releases/app-release.apk">
-								<Button size="lg" className='bg-blue-900'>Télécharger l&apos;application</Button>
-							</a>
-							<Link href="/shop">
-								<Button size="lg" variant="outline">
-									Voir notre catalogue
-								</Button>
-							</Link>						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	)
+  return (
+    <section className='py-20'>
+      <div className='flex items-center justify-center bg-[url("https://shadcnblocks.com/images/block/patterns/circles.svg")] bg-cover bg-center py-20 text-center md:p-20'>
+        <div className='container'>
+          <div className='mx-auto max-w-screen-md'>
+            <h1 className='mb-4 text-3xl font-semibold text-blue-800  md:text-5xl'>
+              Votre boutique de foot est desormais dans votre poche
+            </h1>
+            <p className='md:text-lg'>
+              Découvrez notre collection d&apos;articles de foot et de sport
+              directement depuis votre smartphone. Maillots, chaussures,
+              équipements... Commandez facilement et profitez de nos offres
+              exclusives sur l&apos;application !
+            </p>
+            <div className='mt-11 flex flex-col justify-center gap-2 sm:flex-row'>
+              <a
+                download
+                href='https://s3.ln-foot.com/app-releases/app-release.apk'
+              >
+                <Button size='lg' className='bg-blue-900'>
+                  Télécharger l&apos;application
+                </Button>
+              </a>
+              <Link href='/shop'>
+                <Button size='lg' variant='outline'>
+                  Voir notre catalogue
+                </Button>
+              </Link>{' '}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
